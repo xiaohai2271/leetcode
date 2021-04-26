5cc525ae34ed94491bce4d8f632705f577091914
禾几海 <zh56462271@qq.com>
2021-04-26 23:28:31 +0800

    refactor

--- tools/TransFile.java
+++ tools/TransFile.java
@@ -1,28 +1,34 @@
-import java.io.*;
-
+import java.io.File;
+import java.util.function.Consumer;
 
 public class TransFile {
+
     public static void main(String[] args) {
-        eachFile(new File("../src/cn/celess"));
+        new TransFile().eachFile(new File("."), deleteFile());
+        //        new TransFile().eachFile(new File("."), file -> {
+        //            if (file.getName().endsWith(".java")) {
+        //                System.out.printf("%s\n", file.getName());
+        //            }
+        //        });
     }
 
-    public static void delete(File file) {
-        if (file.getPath().indexOf("-") != -1) {
-            System.out.printf("%b \t %s\n", file.delete(), file.getPath());
-        }
+    public static Consumer<File> deleteFile() {
+        return file -> {
+            System.out.printf("%b%s\n", file.delete(), file.getName());
+        };
     }
 
     /**
      * 遍历文件
-     * @param file
      */
-    public static void eachFile(File file) {
+    public void eachFile(File file, Consumer<File> fileConsumer) {
+        if (file == null) return;
         if (file.isFile()) {
-            delete(file);
+            fileConsumer.accept(file);
             return;
         }
-        for (File TFile : file.listFiles()) {
-            eachFile(TFile);
-        }
+        File[] files = file.listFiles();
+        if (files == null) return;
+        for (File TFile : files) eachFile(TFile, fileConsumer);
     }
 }
